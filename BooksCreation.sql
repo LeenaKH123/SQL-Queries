0aae735da17533a05ccc9ce9785b87a416539bf4
leenakhalili <engleena@yahoo.com>
2021-10-12 15:36:18 +0300

    Books table created

---
+++ BooksCreation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Books (
+    Bookid int NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    Authorid int,
+    PRIMARY KEY (Bookid)
+    FOREIGN KEY (Authorid) REFERENCES Authors(Authorid)
+);
