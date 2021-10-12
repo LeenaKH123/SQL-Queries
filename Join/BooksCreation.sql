1607156cf0765846bd812c8c60195ad340b41a80
leenakhalili <engleena@yahoo.com>
2021-10-12 15:49:41 +0300

    joining and inserting

--- Join/BooksCreation.sql
+++ Join/BooksCreation.sql
@@ -2,6 +2,6 @@ CREATE TABLE Books (
     Bookid int NOT NULL AUTO_INCREMENT,
     title varchar(255) NOT NULL,
     Authorid int,
-    PRIMARY KEY (Bookid)
+    PRIMARY KEY (Bookid),
     FOREIGN KEY (Authorid) REFERENCES Authors(Authorid)
 );
