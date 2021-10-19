ada96bc125a4eab6257d99eb283416c458eeb9be
leenakhalili <engleena@yahoo.com>
2021-10-19 12:15:21 +0300

    YAY

--- Join-Cumulative/3wayJoin2.sql
+++ Join-Cumulative/3wayJoin2.sql
@@ -1,4 +1,4 @@
 SELECT title, name, rating
-FROM books
-JOIN authors on authors.id = books.author_id
-JOIN reviews on reviews.reviewer_id = authors.id and reviews.book_id = books.id
+FROM Books
+JOIN Authors on Authors.Authorid = Books.Authorid
+JOIN reviews on reviews.reviewer_id = Authors.Authorid and reviews.book_id = Books.Bookid
