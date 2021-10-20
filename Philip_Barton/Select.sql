2a64f3806c32f109879b5ad62bb3d9b772f2b343
leenakhalili <engleena@yahoo.com>
2021-10-20 12:30:40 +0300

    minor changes

---
+++ Philip_Barton/Select.sql
@@ -0,0 +1,14 @@
+-- query one
+select 1+1 as result
+, 3+3 as result2
+, 4+4 as 'result 3';
+
+-- query two
+select 1+1 as result
+, 3+3 as result2
+, 4+4 as 'result 3'
+from transactions
+
+-- query three
+select * from transactions
+
--- Philip_Barton/basicSelect.sql
+++
@@ -1,6 +0,0 @@
--- query one
-select 1+1 as result
-, 3+3 as result2
-, 4+4 as 'result 3';
-
--- query two
