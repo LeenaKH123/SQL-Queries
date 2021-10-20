e269743507199d7491f21968b4ea9032d69510a4
leenakhalili <engleena@yahoo.com>
2021-10-20 13:56:46 +0300

    group by

--- Philip_Barton/groupBy.sql
+++ Philip_Barton/groupBy.sql
@@ -1,3 +1,11 @@
+----group by with aggragte function---
 select PurchaseOrderID, sum(LineTotal)
 from transactions
 group by PurchaseOrderID
+
+-- second query ------adding where-- and alias
+select PurchaseOrderID, sum(LineTotal) as "Total of line total"
+from transactions
+where PurchaseOrderID = 7
+group by PurchaseOrderID
+
