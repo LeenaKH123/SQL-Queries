dd097bb0f2ba8807a85381d888d43ee574e8ef82
leenakhalili <engleena@yahoo.com>
2021-10-20 13:30:08 +0300

    selecting blue products

--- Philip_Barton/where.sql
+++ Philip_Barton/where.sql
@@ -4,4 +4,7 @@ select * from transactions where PurchaseOrderID >= 10 or PurchaseOrderID = 8;
 -- 2nd query --
 select * from transactions where PurchaseOrderID in (1,8)
 
+--3rd query - select blue products --
+select * from Product where Color = "blue";
+
 
