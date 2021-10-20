f7476b871707a25e4dfdc15184a9878837400390
leenakhalili <engleena@yahoo.com>
2021-10-20 13:20:06 +0300

    done with joins

--- Philip_Barton/Join.sql
+++ Philip_Barton/Join.sql
@@ -7,3 +7,9 @@ on transactions.ProductID = Product.ProductID
 select * from transactions as T
 left join Product as P
 on T.ProductID = P.ProductID
+
+--- adding more columns to the join
+select PurchaseOrderID, OrderQty, ProductID, UnitPrice, LineTotal from transactions as T
+left join Product as P
+on T.ProductID = P.ProductID
+
