27bc6646829fd826e50e8f0a8f6ad283f3f42a2e
leenakhalili <engleena@yahoo.com>
2021-10-21 16:24:57 +0300

    subquery with join

--- Assembling_Queries_Subqueries/subquery3.sql
+++ Assembling_Queries_Subqueries/subquery3.sql
@@ -1,6 +1,14 @@
 -- calculate the average price of phones for each manufacturer
 -- print the highest average price
-select MAX(p.avg_price)
+select MAX(p.avg_price) AS max_average_price
 FROM (select AVG(price) as avg_price
 from Phones
 group by manufacturer) AS p;
+
+-- subquery with join -- 
+select first_name
+from users
+JOIN (
+    select user_id from orders where product_id = 3
+) AS o
+ON o.user_id = users.id;
