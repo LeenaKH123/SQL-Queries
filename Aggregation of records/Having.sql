ec16722e371b72df9240b5f9b8565c992892e59e
leenakhalili <engleena@yahoo.com>
2021-10-21 08:55:44 +0300

    having in action

--- Aggregation of records/Having.sql
+++ Aggregation of records/Having.sql
@@ -1,2 +1,7 @@
 --A HAVING clause in SQL specifies that an SQL SELECT statement must only return rows where aggregate values meet
 -- the specified conditions
+select photo_id, count(*)
+from comments
+where photo_id < 3
+group by photo_id
+HAVING count(*) > 2;
