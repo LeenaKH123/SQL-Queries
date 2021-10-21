5fccf0f9f7e0626f3481f3e4254ec1d220c3c9c9
leenakhalili <engleena@yahoo.com>
2021-10-21 14:51:18 +0300

    the magical subquery

--- Assembling_Queries_Subqueries/subquery2.sql
+++ Assembling_Queries_Subqueries/subquery2.sql
@@ -4,3 +4,15 @@
 select name, price, price / (select max(price) from Phones) as price_ratio
 from Phones
 --group by name, price
+
+--2nd query
+select name, price / weight as price_weight_ratio
+from products;
+
+--3rd query
+select name, price_weight_ratio
+from (
+select name, price / weight As price_weight_ratio
+from products
+) as p
+where price_weight_ratio > 5;
