8321450170c529adc97367752c657d6b87711564
leenakhalili <engleena@yahoo.com>
2021-10-21 09:19:53 +0300

    having in progress

--- Aggregation of records/Having.sql
+++ Aggregation of records/Having.sql
@@ -5,3 +5,12 @@ from comments
 where photo_id < 3 -- photo id < 3
 group by photo_id
 HAVING count(*) > 2; -- the photo has more than two comments
+
+-- another having example
+-- find all the users(user_ids) where the user has commented on the first 50 photos -- meaning find the users with photo_id < 50
+-- and the user added more than two comments on those photos
+SELECT user_id, COUNT(*)
+FROM comments
+where photo_id < 50
+group by user_id
+having count(*) > 20;
