dffb3b21e83cf58bab166b78e5f7d4be02cbc8c6
leenakhalili <engleena@yahoo.com>
2021-10-21 11:54:48 +0300

    sorting asc desc offset limit

--- SortingRecords/sort.sql
+++ SortingRecords/sort.sql
@@ -3,3 +3,16 @@ select * from products order by price DESC;
 
 -- 2nd query
 select * from products order by price ASC;
+
+-- 3rd query
+select * from products order by price, weight DESC;
+
+-- offset 3 : skip the first three rows of the result set
+-- limit 2 : only give the first two rows of the result set
+select * from users limit 50;
+
+select * from products order by price DESC limit 20 offset 40;
+
+
+
+
