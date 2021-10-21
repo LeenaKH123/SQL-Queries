c6eff8e5dc85037ef04a7dbddb07b4000280bb55
leenakhalili <engleena@yahoo.com>
2021-10-21 16:14:51 +0300

    the magical query 4

--- Assembling_Queries_Subqueries/subquery3.sql
+++ Assembling_Queries_Subqueries/subquery3.sql
@@ -1,5 +1,6 @@
 -- calculate the average price of phones for each manufacturer
 -- print the highest average price
-select AVG(price) as avg_price
+select MAX(p.avg_price)
+FROM (select AVG(price) as avg_price
 from Phones
-group by manufacturer
+group by manufacturer) AS p;
