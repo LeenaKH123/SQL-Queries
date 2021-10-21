08cc3fd3ed013c2d81806e76641fa2c025fc9cce
leenakhalili <engleena@yahoo.com>
2021-10-21 12:30:27 +0300

    union in progress

--- UnionsAndIntersections/union.sql
+++ UnionsAndIntersections/union.sql
@@ -1,3 +1,11 @@
+-- union notes
+-- unoin join together the reuslts of two queries and remove duplicate rows
+-- union all : join together results of two queries
+-- intersect : find the common rows in the results of two queries
+-- intersect all : find the rows common in the results of two queries
+-- except : find the rows that are present in first query but not second query - remove duplicates
+-- except all : find the rows that are present in first query but not second query
+
 -- find the 4 products with the highest price and the 4 products with the highest price/weight ratio
 (
     select *
