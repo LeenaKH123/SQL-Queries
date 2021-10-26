3b927532e6b92cec75b665bfe29109ba79a2c228
leenakhalili <engleena@yahoo.com>
2021-10-27 01:46:10 +0300

    subquery 4

--- Assembling_Queries_Subqueries/subquery4.sql
+++ Assembling_Queries_Subqueries/subquery4.sql
@@ -1,4 +1,14 @@
 -- queries that are used inside ebay
 select id from orders
 where product_id
-IN (select id from products where price / weight > 5)
+IN (select id from products where price / weight > 50)
+
+--- select products with price greator than avg price
+select name, price
+from products
+where price > (
+    select avg(price) from products
+)
+order by price
+
+
