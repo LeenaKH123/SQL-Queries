b6862397622f5e32eebc9b73a608c09aa94b8d6a
leenakhalili <engleena@yahoo.com>
2021-10-27 03:58:59 +0300

    __

--- Assembling_Queries_Subqueries/correlated_subquries.sql
+++ Assembling_Queries_Subqueries/correlated_subquries.sql
@@ -1,3 +1,9 @@
+-- first way to write the query
 select name, department, price
 from products
 where price < (select max(price) from products where department = 'Industrial')
+
+-- second way to write the query
+select name, department, price
+from products as p1
+where p1.price < (select max(price) from products where department = 'Industrial')
