8cd148c7171ed83d3125b1f36eec0d4883f3edf2
leenakhalili <engleena@yahoo.com>
2021-10-27 05:05:56 +0300

    __

--- Assembling_Queries_Subqueries/correlated_subquries.sql
+++ Assembling_Queries_Subqueries/correlated_subquries.sql
@@ -7,3 +7,24 @@ where price < (select max(price) from products where department = 'Industrial')
 select name, department, price
 from products as p1
 where p1.price < (select max(price) from products where department = 'Industrial')
+
+-- third way to write the query
+select name, department, price
+from products as p1
+where p1.price < (select max(price) from products as p2 where p1.department = p2.department)
+
+-- 4
+select name, department, price
+from products as p1
+where p1.price = (
+    select max(price)
+    from products AS p2
+    where p1.department = p2.department
+)
+
+--- more on correlated sub queries
+--without using a join or a group by print the number of orders for each product
+-- this is like double nested for loop
+select p1.name, (select count(*) from orders as o1 where o1.product_id = p1.id) as num_orders
+from products as p1
+
