aa28d4e5b7c3c2e02cf0bf885e3048001fbf0cfe
leenakhalili <engleena@yahoo.com>
2021-10-27 05:21:37 +0300

    the magical query

--- Assembling_Queries_Subqueries/SelectNoFrom.sql
+++ Assembling_Queries_Subqueries/SelectNoFrom.sql
@@ -7,3 +7,8 @@ select (
 select (
     select max(price) from products
 ) as max1, (select avg (price) from products) as avg1
+
+-- using only subqueries, print the max price, min price, and avg price for all phones
+-- rename each column to min_price, max_price, avg_price
+
+select (select max(price) from Phones) as max_price, (select min(price) from Phones) as min_price, (select avg(price) from Phones) as avg_price
