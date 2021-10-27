9ed5cca62cf2e08baee5668fca2f3ffd852d3d9c
leenakhalili <engleena@yahoo.com>
2021-10-27 03:28:53 +0300

    subqueries variation

--- Assembling_Queries_Subqueries/subquery4.sql
+++ Assembling_Queries_Subqueries/subquery4.sql
@@ -17,6 +17,19 @@ select name, price
 from Phones 
 where price > (select price from Phones where name like "%Monte%")
 
+-- another example, not valid subquery as the operand should contain one column
+SELECT price, (SELECT manufacturer, units_sold FROM Phones)
+FROM Phones;
+
+-- another example
+SELECT (price * units_sold) 
+FROM (SELECT * FROM Phones) as p;
+
+-- another example
+SELECT price * units_sold
+FROM Phones
+WHERE price > ALL (SELECT price FROM Phones WHERE manufacturer = 'Nokia');
+
 
 
 
